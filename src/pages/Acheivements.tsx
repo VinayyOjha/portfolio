bfa6e77ecd6dfaa83b9cdbb66ba5dd50e132aaf2
VinayyOjha <ojhavinay7602@.com>
2025-07-13 09:42:20 +0530

    intialksed tailwind and set the layout

---
+++ src/pages/Acheivements.tsx
@@ -0,0 +1,7 @@
+
+
+export const Acheivements = () => {
+  return (
+    <div>Acheivements</div>
+  )
+}
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,25 @@
-
+import { Acheivements } from "./Acheivements";
+import { Blogs } from "./Blogs";
+import Intro from "./Intro";
+import Nav from "./Nav";
+import Projects from "./Projects";
+import { Skills } from "./Skills";
 
 const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div className="min-h-screen bg-[#0f0f0f]">
+      <div className="container min-h-screen max-w-2xl mx-auto border border-white/30">
+        <div className="flex flex-col gap-16">
+          <Nav />
+          <Intro />
+          <Projects />
+          <Blogs />
+          <Skills />
+          <Acheivements />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Landing
+export default Landing;
