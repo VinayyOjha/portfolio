0bb1a8226f05c47c320fc40c82854322742b72c5
VinayyOjha <ojhavinay7602@.com>
2025-09-27 06:44:34 +0530

    ui fix

--- src/components/IntroForMobile.tsx
+++ src/components/IntroForMobile.tsx
@@ -2,7 +2,7 @@ import pfp from "../assets/PFP.png";
 
 export const IntroForMobile = () => {
   return (
-    <div className="sm:hidden p-2 w-full flex justify-between rounded-md border border-white/40">
+    <div className="mb-2 sm:hidden p-2 w-full flex justify-between items-center rounded-md border border-white/40">
         <div className="">
             <img 
                 src={pfp} 
